6aa92d4dc2b33313763537ba39e22ee9e2173a7a
Bartolome Chavez Campano <bartolomechavezcampano2@gmail.com>
2022-05-13 17:23:16 -0300

    feature add log to socket path

--- src/database/sequelize/postgresql/postgres.client.js
+++ src/database/sequelize/postgresql/postgres.client.js
@@ -2,6 +2,8 @@ const { Sequelize } = require("sequelize");
 const dbConfig = require("../../config/config");
 const appConfig = require("../../../config/env.config");
 
+console.info(`Database socket path -> ${process.env.DB_SOCKET_PATH}/${process.env.INSTANCE_CONNECTION_NAME}`)
+
 const options = {
     dialect: "postgres",
     host: dbConfig.host,
@@ -12,7 +14,7 @@ const options = {
 if (appConfig.enviroment == "production" && dbConfig.provider == "GCP") {
     options.host = `${process.env.DB_SOCKET_PATH}/${process.env.INSTANCE_CONNECTION_NAME}`;
     options.dialectOptions = {
-        socketPath: process.env.DB_SOCKET_PATH
+        socketPath: `${process.env.DB_SOCKET_PATH}/${process.env.INSTANCE_CONNECTION_NAME}`
     };
 }
 
